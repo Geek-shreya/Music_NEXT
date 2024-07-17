5faf6a64d07ef57c0cda4fc885a500b848e80fc7
Geek-shreya <gargshreya003@gmail.com>
2024-07-18 01:15:59 +0530

    completed

--- music/src/app/page.tsx
+++ music/src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instrustors from "@/components/Instrustors";
+import MusicMovingCards from "@/components/MovingCards";
+import UpcomingWebniars from "@/components/UpcomingWebniars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Shreyya Garg</h1>
+      <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicMovingCards/>
+      <UpcomingWebniars/>
+      <Instrustors/> 
+      <Footer/>
     </main>
   );
 }
